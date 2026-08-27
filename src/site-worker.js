d74285cc63aa2a488741c3e1affbc3768e3d0712
Animesh Patel <g57436746@gmail.com>
2026-08-27 12:41:54 +0200

    Serve the Vite app at the Site root

--- src/site-worker.js
+++ src/site-worker.js
@@ -4,6 +4,11 @@ export default {
       return new Response("Static asset binding unavailable", { status: 500 });
     }
 
-    return env.ASSETS.fetch(request);
+    const url = new URL(request.url);
+    if (url.pathname === "/") {
+      url.pathname = "/index.html";
+    }
+
+    return env.ASSETS.fetch(new Request(url, request));
   },
 };
